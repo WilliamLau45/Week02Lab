6a7e98c2e5bb4c3776a1d4df42d269e143a899f6
ohiow <ohiow@10.0.0.187>
2022-09-17 17:34:05 -0600

    New Project Creation Java Application for Week02Lab

---
+++ src/javaweek02lab/JavaWeek02Lab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaweek02lab;
+
+/**
+ *
+ * @author ohiow
+ */
+public class JavaWeek02Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is Week 2 Lab assignment. :D");
+    }
+    
+}
